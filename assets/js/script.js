9cf492cd7f950776a3ff0e3722b0606fff87a936
Katerina El <yyelistratova@gmail.com>
2023-03-22 14:19:07 +0000

    Add event listeners to DOM loading and distance clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,26 @@
-// change active distance choice (code from https://www.w3schools.com/, slightly modified)
+// wait for the DOM finish loading
 
-var distanceItems = document.getElementsByClassName('distance-item');
-for (var item = 0; item < distanceItems.length; item++) {
-    distanceItems[item].addEventListener('click', function() {
-        var current = document.getElementsByClassName('active');
-        current[0].className = current[0].className.replace(' active', '');
-        this.className += ' active';
-    })
-}
+document.addEventListener('DOMContentLoaded', function () {                                 
+    let distanceItems = document.getElementsByClassName('distance-item');  
+            
+     // change distance choice                 
+    for (let item = 0; item < distanceItems.length; item++) {                      // code from www.w3schools.com/, slightly modified   
+        distanceItems[item].addEventListener('click', function () {
+            let current = document.getElementsByClassName('active');
+            current[0].className = current[0].className.replace(' active', '');
+            this.className += ' active';
+        })
+    }
+
+    // set chosen distance
+    for (item of distanceItems) {
+        item.addEventListener('click', function() {
+            if (this.getAttribute('data-distance') === 'input') {
+                alert('You chose input');
+            } else {
+                let setDistance = this.getAttribute('data-distance');
+                alert(`You chose ${setDistance} distance`)
+            }
+        })
+    }
+})
